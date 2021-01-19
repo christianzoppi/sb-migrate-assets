5296d36f230e3efac5b682327c3c2f50c250e3be
Christian Zoppi <me@christianzoppi.com>
2021-01-19 18:13:45 +0100

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+import Migration from './src/index.js'
+import inquirer from 'inquirer'
+
+const questions = [
+  {
+    type: 'input',
+    name: 'oauth',
+    message: "Please enter your OAUTH Token",
+  },
+  {
+    type: 'input',
+    name: 'source_space_id',
+    message: "Please enter the Source Space Id",
+  },
+  {
+    type: 'input',
+    name: 'target_space_id',
+    message: "Please enter the Target Space Id",
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+    const migration = new Migration(answers.oauth, answers.source_space_id, answers.target_space_id)
+    migration.start()
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+### Usage
+Run `npm run start` and provide a OAUTH code from your account, the id of the source space and the id of the target space.  
+The script will replace the url of the assets in the entries and it'll update them keeping the correct publish/unpublish status.
